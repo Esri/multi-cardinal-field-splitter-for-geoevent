5b287123b8793ad10dba2ca33f40716d2a1e49b5
Morakot Pilouk <mpilouk@esri.com>
2016-12-01 16:57:25 -0800

    Updated to 10.5
    Updated to 10.5

--- multicardinal-field-splitter-processor/src/main/java/com/esri/geoevent/processor/multicardinalfieldsplitter/MulticardinalFieldSplitterDefinition.java
+++ multicardinal-field-splitter-processor/src/main/java/com/esri/geoevent/processor/multicardinalfieldsplitter/MulticardinalFieldSplitterDefinition.java
@@ -27,7 +27,7 @@ public class MulticardinalFieldSplitterDefinition extends GeoEventProcessorDefin
   @Override
   public String getVersion()
   {
-    return "10.4.0";
+    return "10.5.0";
   }
 
   @Override
